82d4b0e079236355b89a0bd14d572b90bfca6e72
Temi Dosu <tdosu@smu.edu>
2022-09-30 14:18:30 -0500

    Installed MUI/icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import SearchBar from 'material-ui-search-bar';
 
+function doSomethingWith(value) {
+
+}
 
 function App() {
 	return (
 		<>
 			<SearchBar
-				value={this.state.value}
+				value={''}
 				onChange={(newValue) => this.setState({ value: newValue })}
 				onRequestSearch={() => doSomethingWith(this.state.value)}
 			/>
+			<p>Hello</p>
 		</>
 	);
 }
