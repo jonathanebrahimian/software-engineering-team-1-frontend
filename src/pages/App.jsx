b5c5c2178707da69d6e77d316548bfb1af8386db
Temi Dosu <tdosu@smu.edu>
2022-09-30 14:34:24 -0500

    Fix blank page

---
+++ src/pages/App.jsx
@@ -0,0 +1,16 @@
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { Link } from "react-router-dom";
+// import SearchBar from 'material-ui-search-bar';
+import Button from '@mui/material/Button';
+import * as React from 'react';
+
+function App() {
+	return (
+		<>
+			<p>Hi</p>
+			{/* <Button variant="contained">Hello World</Button>; */}
+		</>
+	);
+}
+
+export default App
