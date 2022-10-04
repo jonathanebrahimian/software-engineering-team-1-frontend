4d6eca3e05c7ab55217fa5e8100e66d2d597ee50
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-04 15:09:14 -0500

    Introduced PublicationTable component

---
+++ src/components/PublicationTable.jsx
@@ -0,0 +1,21 @@
+/**
+ * Author:          Kassi Bertrand
+ * filename:        PublicationTable.jsx
+ * Date:            10/04/2022
+ * Description:     This component displays the 5 most recent
+ *                  TA positions + some other information available
+ *                  to view at a glance
+ */
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function PublicationTable({ positions }) {
+
+}
+
+export default PublicationTable;
