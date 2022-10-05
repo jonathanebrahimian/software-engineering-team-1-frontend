2e78a58bd4301776b2507e6de4eae6661429f2c3
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-04 20:09:46 -0500

    feat: Added PublicationTable component to the Admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -5,7 +5,27 @@ import Grid from "@mui/material/Grid";
 import { Divider, Typography } from "@mui/material";
 import ApplicationCard from '../components/ApplicationCard';
 import PublicationCard from '../components/PublicationCard'
+import PublicationTable from '../components/PublicationTable';
 
+
+//hypothetical data fetched from server
+//To be removed, when mock API is operational
+function createData(
+    className,
+    section,
+    id,
+    applications,
+    status
+) {
+    return { className, section, id, applications, status };
+}
+const positions = [
+    createData('CS 1341', 'N14', '112@1', 5, 'closed'),
+    createData('CS 1342', 'N14', 'ascs1', 4, 'open'),
+    createData('CS 2341', 'N14', 'ct10z', 2, 'open'),
+    createData('CS 1340', 'N14', 'fsfz2', 3, 'closed'),
+    createData('CS 2240', 'N14', '34zf2', 1, 'closed'),
+]
 function AdminPage() {
     return (
         <Stack direction='column'>
@@ -23,7 +43,10 @@ function AdminPage() {
                     </Grid>
                 </Grid>
                 <Grid item xs={12} sx={{ mx: 7, mt: 4 }}>
-                    <Divider>Recently Published Positions (3)</Divider>
+                    <Divider>Recently Published Positions (5)</Divider>
+                </Grid>
+                <Grid item xs={12} sx={{ mx: 7, mt: 4 }}>
+                    <PublicationTable positions={positions} />
                 </Grid>
             </Grid>
         </Stack >
