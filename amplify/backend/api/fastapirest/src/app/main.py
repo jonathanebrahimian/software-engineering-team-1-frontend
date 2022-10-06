35f57603d676ba821e811be02e4ac88eece01667
jonathanebrahimian <jebrahimian@smu.edu>
2022-10-06 09:04:08 -0500

    Adding classes api

--- amplify/backend/api/fastapirest/src/app/main.py
+++ amplify/backend/api/fastapirest/src/app/main.py
@@ -1,25 +1,20 @@
 # main.py
 
-from typing import Optional
+from typing import Optional, Union, List
 from fastapi import FastAPI
 import boto3
 import os
-
-
-
-from typing import Union, List
 from pydantic import BaseModel
-import datetime
+from datetime import datetime
 
-# class Labs(BaseModel):
-#     start_time: datetime
-#     end_time: datetime
+class Labs(BaseModel):
+    start_time: datetime
+    end_time: datetime
 
-# class Item(BaseModel):
-#     class_id: str
-#     description: Union[str, None] = None
-#     labs: List[Labs] = []
-#     tax: Union[float, None] = None
+class Class(BaseModel):
+    class_id: str
+    description: Union[str, None] = None
+    labs: List[Labs] = []
 
 ENV = os.environ.get("ENV","local")
 
@@ -38,27 +33,16 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World!!"}
 
-@app.get("/tables")
-def tables():
-    print("resources")
-    response = classes_table.put_item(Item={'class_id':'CS 43451','description':'this is a description'})
-    print(response)
-    return response
+@app.get("/classes")
+def tables(class_id: str):
+    response = classes_table.get_item(Key={'class_id':class_id})
+    return response['Item']
 
 @app.post("/classes")
-def classes():
-    pass
+def classes(item: Class):
+    item = item.dict()
+    for x in item['labs']:
+        x['start_time'] = x['start_time'].isoformat()
+        x['end_time'] = x['end_time'].isoformat()
 
-@app.get("/see")
-def see():
-    print("here is the put")
-    response = classes_table.get_item(Key={'class_id':'CS 43451'})
-    print(response)
-    return response
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-
+    return classes_table.put_item(Item=item)
