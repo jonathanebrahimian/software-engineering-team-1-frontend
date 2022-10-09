e9daccf323e85e8f8f8abac1868c0efe13a44373
Kassi Bertrand <bertran.billy@gmail.com>
2022-10-09 17:47:19 -0500

    Added Email and Password field

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,85 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Icon } from '@mui/material';
+
+function handleSubmit(event) {
+    event.preventDefault();
+    //post data
+}
+
+const theme = createTheme();
+
 function LoginPage() {
     return (
-        <div></div>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'white' }}>
+                        <Box
+                            component="img"
+                            sx={{
+                                height: 30,
+                                width: 30
+                            }}
+                            alt="SMU icon"
+                            src="https://raw.githubusercontent.com/jonathanebrahimian/software-engineering-team-1-frontend/master/src/lyle_logo.png"
+                        />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                </Box>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin='normal'
+                        required
+                        fullWidth
+                        id='email'
+                        label="Email Address"
+                        name='email'
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin='normal'
+                        required
+                        fullWidth
+                        name='password'
+                        label='Password'
+                        type="password"
+                        id="password"
+                        autoComplete='current-password'
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant='contained'
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
 
