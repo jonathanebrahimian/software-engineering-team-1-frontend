14a1e5a8f26e7e455e95ea23056021266770335a
Temi Dosu <tdosu@smu.edu>
2022-10-14 14:54:48 -0500

    Use mongodb container backend

--- src/api/classesApi.ts
+++ src/api/classesApi.ts
@@ -1,9 +1,9 @@
 // When the frontend is running in amplify, use the amplify domain. Otherwise, use json server
-const BACKEND_URL = process.env.ENV ? 'http://localhost:5000' : "http://localhost:5000";
+const BACKEND_URL = process.env.ENV ? 'http://localhost:5000' : "http://localhost:8000";
 
 
 export const fetchClasses = async () => {
-	const res = await fetch(`${BACKEND_URL}/classes`);
+	const res = await fetch(`${BACKEND_URL}/class`);
 	const data = await res.json();
 	return data;
 }
